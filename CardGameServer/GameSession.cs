e807fada12acf371c93bd6f3d7f53dd2af94eeb2
IndigoCarmine <tamanisikaminai@gmail.com>
2021-02-24 02:22:03 +0900

    プロジェクト ファイルを追加します。

---
+++ CardGameServer/GameSession.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameServer
+{
+    public class GameSession : Server
+    {
+
+        List<int> Deck = new List<int>();
+        int TopDrawCard = 40;//初期設定で裏面にしておく
+
+        List<Player> playerList = new List<Player>();
+
+        public GameSession(int port) : base(port)
+        {
+            //数字カードを追加
+            for(int i = 0; 40> i; i++)
+            {
+                Deck.Add(i);
+                Deck.Add(i);
+                Deck.Add(i);
+                Deck.Add(i);
+            }
+
+        }
+         
+        public void GameStart()
+        {
+            //人数確認
+            if (playerList.Count != 2) return;
+
+            foreach(Player player in playerList)
+            {
+                Distribute(player, 7);
+                SendData(player, SendParameter.Hand, player.GetHand());
+                SendData(player, SendParameter.OpponentName, GetOtherPlayers(player)[0].Name);
+            }
+
+            SendData(playerList[0], SendParameter.Turn, "0");
+            playerList[0].Turn = true;
+            return;
+
+        }
+
+
+
+
+
+        Random random = new Random();
+        /// <summary>
+        /// カードを配る
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="CardCount">枚数</param>
+        void Distribute(Player player, int CardCount)
+        {
+            for (int i = 0; i < CardCount; i++)
+            {
+                int index = random.Next(0, Deck.Count - 1);
+                player.Hand.Add(Deck[index]);
+                Deck.RemoveAt(index);
+            }
+        }
+
+
+        Player GetPlayer(Socket socket)
+        {
+            
+            foreach(Player player in playerList)
+            {
+                if (player.socket == socket) return player;
+            }
+
+            return null;
+        }
+        List<Player> GetOtherPlayers(Player player)
+        {
+            List<Player> _playerlList = new List<Player>(playerList);
+            _playerlList.Remove(player);
+            return _playerlList;
+        }
+
+        void SendDataAllPlayer(SendParameter Parameter, string Content)
+        {
+            foreach(Player player in playerList)
+            {
+                SendData(player, Parameter, Content);
+            }
+        }
+        void SendData(Socket clientSocket, SendParameter Parameter, string Content)
+        {
+            string data = Parameter.ToString("G") + "=" + Content;
+            base.Send(clientSocket, data);
+        }
+        void SendData(Player player, SendParameter Parameter, string Content)
+        {
+            SendData(player.socket, Parameter, Content);
+        }
+        enum SendParameter
+        {
+            OpponentName,
+            Hand,
+            OpponentHand,
+            Turn,
+            Error,
+            Finish,
+            Start,
+            DrawPile
+        }
+
+        protected override void OnConnectNewClient(StateObject stateObject)
+        {
+            
+            if (playerList.Count > 2)
+            {
+                SendData(stateObject.ClientSocket, SendParameter.Error, "OverCapacity");
+
+            }
+            else
+            {
+                playerList.Add(new Player(stateObject.ClientSocket));
+            }
+
+            if(playerList.Count == 2)
+            {
+                GameStart();
+            }
+            
+
+        }
+
+        protected override void OnDisconnect(StateObject stateObject)
+        {
+            playerList.Remove(GetPlayer(stateObject.ClientSocket));
+            SendDataAllPlayer(SendParameter.Error, "OpponentDisconnect");
+            Console.WriteLine("Disconnect");
+        }
+
+        protected override void OnReceive(StateObject stateObject, string Msg)
+        {
+            Player player = GetPlayer(stateObject.ClientSocket);
+            Player opponent = default;
+            
+            if (GetOtherPlayers(player).Count != 0)
+            {
+                opponent = GetOtherPlayers(player)[0];
+            }
+            
+
+            string[] MsgSplit = Msg.Split('=');
+            string Parameter = MsgSplit[0];
+            string Content = MsgSplit[1];
+
+            switch (Parameter)
+            {
+                case "PlayerName":
+                    if (!player.SetName(Content)) SendData(player, SendParameter.Error, "AlreadySetPlayerName");
+                    break;
+                case "Select":
+                    GameProsess(player, Content);
+                    break;
+                case "Draw":
+                    if (player.Turn)
+                    {
+                        Distribute(player, int.Parse(Content));
+                    }
+                    Distribute(player, int.Parse(Content));
+                    SendData(player, SendParameter.Hand, player.GetHand());
+                    break;
+                case "Uno":
+                    if (player.Hand.Count == 1)
+                    {
+                        player.UnoFrag = true;
+                    }
+                    else
+                    {
+                        SendData(player, SendParameter.Error, "UnExpectedError");
+                    }
+
+                    break;
+                case "DataRequest":
+                    switch (Content)
+                    {
+                        case "OpponentName":
+                            SendData(player, SendParameter.OpponentName, opponent.Name);
+                            break;
+                        case "Hand":
+                            SendData(player, SendParameter.Hand, player.GetHand());
+                            break;
+                        case "OpponentHand":
+                            SendData(player, SendParameter.OpponentHand, opponent.GetOpenedHand());
+                            break;
+                        default:
+                            SendData(player, SendParameter.Error, "UnexpectedError");
+                            break;
+                    }
+                    break;
+                default:
+                    SendData(player, SendParameter.Error, "UnexpectedError");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Selectで送られてきたデータの処理と次のプレイヤーへの受け渡し。
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="Content">CardIDの,区切り</param>
+        void GameProsess(Player player, string Content)
+        {
+            player.Turn = false;
+
+            string[] CardIDArray = Content.Split(',');
+            //Playerの手札と照合
+            List<int> PlayerHand = player.Hand;
+            bool NotExist = false;
+            foreach(string CardID in CardIDArray)
+            {
+                if (!PlayerHand.Exists(x => x == int.Parse(CardID)))
+                {
+                    NotExist = true;
+                    break;
+                }
+                PlayerHand.Remove(int.Parse(CardID));
+            }
+            //選択したカードに手札以外が含まれている。
+            if (NotExist)
+            {
+                SendData(player, SendParameter.Hand, player.GetHand());
+                player.Turn = true;
+                SendData(player, SendParameter.Turn, "0");
+            }
+            if (IsAppropriate(CardIDArray))
+            {
+                SendDataAllPlayer(SendParameter.DrawPile, Content);
+                TopDrawCard = int.Parse(CardIDArray[CardIDArray.Length - 1]);
+                player.Hand = PlayerHand;
+                SendData(player, SendParameter.Hand, player.GetHand());
+                if(player.Hand.Count == 0)
+                {
+                    if (player.UnoFrag)
+                    {
+                        SendData(player, SendParameter.Finish, "Win");
+                        SendData(GetOtherPlayers(player)[0], SendParameter.Finish, "Lose");
+                        Console.WriteLine("We Finish the Game.");
+                    }
+                    else
+                    {
+                        //Uno宣言なしによるペナルティ
+                        Distribute(player, 1);
+                        SendData(player, SendParameter.Hand, player.GetHand());
+                        GetOtherPlayers(player)[0].Turn = true;
+                        SendData(GetOtherPlayers(player)[0], SendParameter.Turn, "0");
+
+                    }
+                }
+                else
+                {
+                    GetOtherPlayers(player)[0].Turn = true;
+                    SendData(GetOtherPlayers(player)[0], SendParameter.Turn, "0");
+                }
+                
+            }
+            else
+            {
+                SendData(player, SendParameter.Hand, player.GetHand());
+                GetOtherPlayers(player)[0].Turn = true;
+                SendData(player, SendParameter.Turn, "0");
+            }
+
+        }
+
+        bool IsAppropriate(string[] CardIDArray)
+        {
+
+            ///ルール通り何かつける。
+            return true;
+        }
+
+
+    }
+
+    public class Player
+    {
+        public Socket socket { get; private set; }
+        public string Name { get; private set; } = "";
+        public List<int> Hand = new List<int>();
+        public List<int> OpenedHand = new List<int>();
+        public bool UnoFrag = false;
+        public bool Turn = false;
+
+        public Player(Socket _socket)
+        {
+            socket = _socket;
+        }
+
+        public bool SetName(string _Name)
+        {
+            if(Name == "")
+            {
+                Name = _Name;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string GetHand()
+        {
+            return string.Join(",", Hand);
+        }
+        public string GetOpenedHand()
+        {
+            return string.Join(",", OpenedHand);
+        }
+    }
+}
---
+++ CardGameServer/Server.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://webbibouroku.com/Blog/Article/socket-server から引用また、一部改変
+/// </summary>
+
+
+namespace CardGameServer
+{
+    abstract public class Server
+    {
+        // スレッド待機用
+        private ManualResetEvent AllDone = new ManualResetEvent(false);
+
+        // サーバーのエンドポイント
+        public IPEndPoint IPEndPoint { get; }
+
+        // 接続中のクライアント(スレッドセーフコレクション)
+        public SynchronizedCollection<Socket> ClientSockets { get; } = new SynchronizedCollection<Socket>();
+        IPAddress ip = IPAddress.Parse("192.168.0.6");
+
+        public Server(int port)
+        {
+            this.IPEndPoint = new IPEndPoint(ip, port);
+        }
+
+        // サーバー起動
+        public void Run()
+        {
+            using (var listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                // Ctrl+Cが押された場合はソケットを閉じる
+                Console.CancelKeyPress += (sender, args) =>
+                {
+                    foreach (var clientSocket in this.ClientSockets) clientSocket?.Close();
+                };
+
+                // ソケットをアドレスにバインドする
+                listenerSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                listenerSocket.Bind(this.IPEndPoint);
+
+                // 接続待機開始
+                listenerSocket.Listen(10);
+                Console.WriteLine($"サーバーを起動しました ... [{listenerSocket.LocalEndPoint}]");
+                Console.WriteLine($"Ctrl + C で中止します ...");
+
+                // 接続待機のループ
+                while (true)
+                {
+                    AllDone.Reset();
+                    listenerSocket.BeginAccept(new AsyncCallback(AcceptCallback), listenerSocket);
+                    AllDone.WaitOne();
+                }
+            }
+        }
+
+        // 接続受付時のコールバック処理
+        private void AcceptCallback(IAsyncResult asyncResult)
+        {
+            // 待機スレッドが進行するようにシグナルをセット
+            AllDone.Set();
+
+            // ソケットを取得
+            var listenerSocket = asyncResult.AsyncState as Socket;
+            var clientSocket = listenerSocket.EndAccept(asyncResult);
+
+            // 接続中のクライアントを追加
+            ClientSockets.Add(clientSocket);
+            Console.WriteLine($"接続: {clientSocket.RemoteEndPoint}");
+            
+
+            // StateObjectを作成
+            var state = new StateObject();
+            state.ClientSocket = clientSocket;
+            OnConnectNewClient(state);
+
+
+            // 受信時のコードバック処理を設定
+            clientSocket.BeginReceive(state.Buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+        }
+
+        private void ReceiveCallback(IAsyncResult asyncResult)
+        {
+            // StateObjectとクライアントソケットを取得
+            var state = asyncResult.AsyncState as StateObject;
+            var clientSocket = state.ClientSocket;
+
+            // クライアントソケットから受信データを取得終了
+            int bytes = clientSocket.EndReceive(asyncResult);
+
+            if (bytes > 0)
+            {
+                // 受信した文字列を表示
+                var content = Encoding.UTF8.GetString(state.Buffer, 0, bytes);
+                Console.WriteLine($"受信データ: {content} [{state.ClientSocket.RemoteEndPoint}]");
+
+                OnReceive(state,content);
+                
+                // 受信時のコードバック処理を再設定
+                clientSocket.BeginReceive(state.Buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+            }
+            else
+            {
+                // 0バイトデータの受信時は、切断されたとき?
+                clientSocket.Close();
+                this.ClientSockets.Remove(clientSocket);
+                OnDisconnect(state);
+            }
+        }
+
+        // クライアントへのメッセージ送信処理
+        protected void Send(Socket clientSocket, String data)
+        {
+            // 受信データをUTF8文字列に変換し送信
+            var bytes = Encoding.UTF8.GetBytes(data);
+            clientSocket.BeginSend(bytes, 0, bytes.Length, 0, new AsyncCallback(SendCallback), clientSocket);
+        }
+
+        // 送信時のコールバック処理
+        private static void SendCallback(IAsyncResult asyncResult)
+        {
+            try
+            {
+                // クライアントソケットへのデータ送信処理を完了する
+                var clientSocket = asyncResult.AsyncState as Socket;
+                var byteSize = clientSocket.EndSend(asyncResult);
+                Console.WriteLine($"送信結果: {byteSize}バイト [{clientSocket.RemoteEndPoint}]");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // 全クライアントへの送信処理
+        protected void SendAllClient(string data)
+        {
+            foreach (var clientSocket in this.ClientSockets)
+            {
+                Send(clientSocket, data);
+            }
+        }
+
+        abstract protected void OnConnectNewClient(StateObject stateObject);
+        abstract protected void OnDisconnect(StateObject stateObject);
+        abstract protected void OnReceive(StateObject stateObject, string content);
+    }
+
+    public class StateObject
+    {
+        public Socket ClientSocket { get; set; }
+        public const int BufferSize = 1024;
+        public byte[] Buffer { get; } = new byte[BufferSize];
+    }
+}
